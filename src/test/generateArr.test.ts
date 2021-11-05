ef77414d98be33654de207e23d6280578f858d25
lqd1434 <1434288209@qq.com>
2021-11-05 23:23:49 +0800

    :zap:集成jest,提升测试效率

--- src/test/generateArr.test.ts
+++ src/test/generateArr.test.ts
@@ -1,6 +1,5 @@
-import { generateArr } from '../utils/Array/generateArr';
+const { generateArr } = require('../utils/Array/generateArr');
 
-console.warn('------findIndex测试结果------');
-
-const res = generateArr(10, 1);
-console.log(res);
+test('------findIndex测试结果------', () => {
+  expect(generateArr(3, 2)).toStrictEqual([2, 2, 2]);
+});
