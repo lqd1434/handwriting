82d27f51ce863b37fb56b78f5e387ad5ad2fa546
lqd1434 <1434288209@qq.com>
2021-11-06 00:21:50 +0800

    :fix:修复deepEqual返回结果错误的问题

--- src/test/deepEqual.test.ts
+++ src/test/deepEqual.test.ts
@@ -1,13 +1,13 @@
 import { deepEqual } from '../utils/Object/deepEqual';
 
-const obj1 = { name: '1', age: 100, obj: { id: 1 } };
-const obj2 = { name: '1', age: 100, obj: { id: 1 } };
+const obj1 = { name: 'lqd', age: 100, obj: { id: 1 } };
+const obj2 = { name: 'lqd', age: 100, obj: { id: 1 } };
 const obj3 = { name: '2', age: 100 };
 const obj4 = { name: '1', age: 100 };
 
 test('------deepEqual测试结果------', () => {
   expect(deepEqual(obj1, obj2)).toBe(true);
-  expect(deepEqual(obj3, obj4)).toBe(true);
+  expect(deepEqual(obj3, obj4)).toBe(false);
   expect(deepEqual({}, { a: 1 })).toBe(false);
   expect(deepEqual({}, {})).toBe(true);
 });
--- src/utils/Object/deepEqual.ts
+++ src/utils/Object/deepEqual.ts
@@ -3,7 +3,7 @@ export const deepEqual = (obj1: Object, obj2: Object) => {
   if (Object.keys(obj1).length === 0 && Object.keys(obj2).length === 0) {
     return true;
   }
-  for (const key in obj1) {
+  for (let key in obj1) {
     if (key in obj2) {
       if (typeof obj1[key] === 'object') {
         flag = deepEqual(obj1[key], obj2[key]);
@@ -12,6 +12,10 @@ export const deepEqual = (obj1: Object, obj2: Object) => {
       }
     } else {
       flag = false;
+    }
+    //如果不相等立刻返回,避免不必要的比较以及覆盖原值,造成判断错误
+    if (!flag) {
+      break;
     }
   }
   return flag;
