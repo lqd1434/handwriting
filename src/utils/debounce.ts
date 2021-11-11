5dd408a981268b3a63e2e840d92e67e1308c8050
lqd1434 <1434288209@qq.com>
2021-11-11 23:05:06 +0800

    feat:更新防抖函数

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,14 +1,10 @@
-// 这个是用来获取当前时间戳的
-function now() {
-  return +new Date();
-}
 /**
  * 防抖函数，返回函数连续调用时，空闲时间必须大于或等于 wait，func 才会执行
  *
- * @param  {function} func        回调函数
- * @param  {number}   wait        表示时间窗口的间隔
- * @param  {boolean}  immediate   设置为ture时，是否立即调用函数
- * @return {function}             返回客户调用函数
+ * @param  func   回调函数
+ * @param  wait    表示时间窗口的间隔
+ * @param  immediate  设置为ture时，是否立即调用函数
+ * @return  返回客户调用函数
  */
 export function debounce(func: Function, wait: number = 50, immediate: boolean = true) {
   let timer;
